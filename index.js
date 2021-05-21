7026a82ce4a8d27bacb8f87103e9da0528e926e6
kelvinHart1 <berrypaul137@gmail.com>
2021-05-21 09:06:32 -0700

    blog assingnment

---
+++ index.js
@@ -0,0 +1,21 @@
+/*let x = 2
+let y = x + 3
+let z = x+ y
+console.log(y)
+let array1= ['a',2,'b','c',5]
+const person = { // this is an object
+    name: 'master will',
+    age: 57,
+    character:
+    }
+
+array1[0] = 'man'
+console.log(array1)
+console.log(array1.length)
+console.log(person)
+console.log(person.name)
+console.log(person.name.split(" "))*/  
+function addnumber (a,b)
+{
+    var total = a+b;
+} return total;
