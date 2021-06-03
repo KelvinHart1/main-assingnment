9f273cda098b27d8efddfd9ed3e372578d00bb5e
KelvinHart1 <82844915+KelvinHart1@users.noreply.github.com>
2021-06-03 05:38:02 +0100

    Add files via upload

---
+++ LOOP/index.js
@@ -0,0 +1,14 @@
+for (var x=0; x<=15; x++ )
+{
+     if ( x % 2 == 0) {
+          console.log (x + " is even")
+     }
+}
+    
+/* or */
+for ( var  y = 2; y <=15; x+=2 )
+
+{
+    console.log (y + " is even")
+}
+    
